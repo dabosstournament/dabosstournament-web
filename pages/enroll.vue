5356de87c3c51987bc1ae7d4fbbe25733bf43d99
Lorenzo Fiamingo <personal@lorenzofiamingo.com>
2024-08-22 11:21:29 +0200

    Update enroll.vue

--- pages/enroll.vue
+++ pages/enroll.vue
@@ -144,11 +144,9 @@ import { number, object, string, type InferType } from 'yup'
 import type { FormSubmitEvent } from '#ui/types'
 import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
 
-head() {
-    return {
-      title: "DABOSS - Iscrizione"
-    };
-}
+definePageMeta({
+  title: 'DABOSS - Iscrizione'
+})
     
 const cellularRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
