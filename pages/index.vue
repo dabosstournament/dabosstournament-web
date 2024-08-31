b1ee64e1d768950f8fbeb2974d9d6a999415ec89
Lorenzo Fiamingo <personal@lorenzofiamingo.com>
2024-08-31 15:41:49 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
         <UContainer class="text-center text-6xl md:text-8xl lg:text-9xl flex place-content-center select-none">
             <h1><span class="text-8xl lg:text-9xl">DA<span class="text-magenta-700">BOSS</span></span> TOURNAMENT</h1>
         </UContainer>
+        <UContainer class="select-none">
+            <h2 class="text-5xl">SEE Y'ALL NEXT YEAR</h2>
+        </UContainer>
         <!--UContainer class="select-none">
             <UButton to="enroll" size="xl" label="ISCRIVI IL TUO TEAM" />
         </UContainer-->
